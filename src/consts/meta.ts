034539e5b05412ee184d6445a28329707df0f2c8
SatooRu65536 <ryuto65536@gmail.com>
2025-02-02 01:44:37 +0900

    feat: metatag

---
+++ src/consts/meta.ts
@@ -0,0 +1,18 @@
+import ogp from '@/assets/opengraph-image.jpg';
+import favicon from '@/assets/favicon.png';
+
+export const FAVICON: ImageMetadata = favicon;
+export const OGP_IMAGE: ImageMetadata = ogp;
+export const HOST_NAME = 'https://sysken.net/';
+export const SITE_NAME = 'システム工学研究会';
+export const KEY_WORDS = ['システム工学研究会', 'シス研', '愛知工業大学', '愛工大'];
+export const CATEGORY = 'website';
+export const DESCRIPTION =
+  'システム工学研究会ってどういうサークル？' +
+  'システム工学研究会(通称シス研)は愛知工業大学公認の情報系サークルです。' +
+  'このサークルの部室にはプログラミングや電子工作、インフラなどといった様々な情報の分野についての勉強ができる環境が整っています。' +
+  'シス研では勉強会やサークル内ハッカソンを開催したり、大学祭では工科展に出展したりしています。' +
+  '最近では外部のハッカソンに参加する人もいたり、自分の作りたいものを制作するなどして自由に活動しています。' +
+  'またサークルのWebページやサーバーの管理も行っています。';
+
+export const TWITTER_CARD_TYPE = 'summary_large_image';
