42ce7c467b241d1e3108a849733e9aaae5b61fd4
nasubi-dev <r916nis@gmail.com>
2026-01-08 17:49:56 +0900

    SysHack 2026 更新

--- src/utils/images.ts
+++ src/utils/images.ts
@@ -1,24 +1,15 @@
-import bbq from '@/assets/top/bbq.webp';
-import budougariBbq from '@/assets/top/budougari-bbq.webp';
-import gakusai3 from '@/assets/top/gakusai3.webp';
-import syscat from '@/assets/top/syscat.webp';
-import benkyokai from '@/assets/top/benkyokai.webp';
-import budougari from '@/assets/top/budougari.webp';
-import geekcampNagoya2024 from '@/assets/top/geekcamp-nagoya2024.webp';
-import opencampus from '@/assets/top/opencampus.webp';
-import xmas from '@/assets/top/xmas.webp';
-import benkyokai2 from '@/assets/top/benkyokai2.webp';
-import gakusai from '@/assets/top/gakusai.webp';
-import halloween from '@/assets/top/halloween.webp';
-import opencampus2 from '@/assets/top/opencampus2.webp';
-import xmas2 from '@/assets/top/xmas2.webp';
-import benkyokai3 from '@/assets/top/benkyokai3.webp';
-import gakusai2 from '@/assets/top/gakusai2.webp';
-import halloween2 from '@/assets/top/halloween2.webp';
-import retoro from '@/assets/top/retoro.webp';
-import xmas3 from '@/assets/top/xmas3.webp';
-import defaultThumbnail from '@/assets/posts/default.webp';
-import syscat2 from '@/assets/syscat.webp';
+import bbg1 from '@/assets/top/bbq1.png';
+import bbg2 from '@/assets/top/bbq2.png';
+import bbg3 from '@/assets/top/bbq3.jpg';
+import begginershack1 from '@/assets/top/begginershack1.png';
+import begginershack2 from '@/assets/top/begginershack2.jpg';
+import benkyokai from '@/assets/top/benkyokai.png';
+import bowling from '@/assets/top/bowling.jpg';
+import soukai from '@/assets/top/soukai.jpg';
+import syscat from '@/assets/top/syscat.png';
+import undokai1 from '@/assets/top/undokai1.jpg';
+
+const defaultThumbnail = bbg1;
 
 export const PRELOAD_IMAGE_MAP = {
   hero: '/hero.webp',
@@ -26,29 +17,20 @@ export const PRELOAD_IMAGE_MAP = {
 };
 
 export const IMAGE_MAP = {
-  bbq,
-  budougariBbq,
-  gakusai3,
-  syscat,
+  undokai1,
   benkyokai,
-  budougari,
-  geekcampNagoya2024,
-  opencampus,
-  xmas,
-  benkyokai2,
-  gakusai,
-  halloween,
-  opencampus2,
-  xmas2,
-  benkyokai3,
-  gakusai2,
-  halloween2,
-  retoro,
-  xmas3,
+  bowling,
+  bbg1,
+  bbg2,
+  bbg3,
+  begginershack1,
+  begginershack2,
+  soukai,
+  syscat,
 } as const;
 export const IMAGES = Object.values(IMAGE_MAP);
 
 export const DEFAULT_POST_THUMBNAIL = defaultThumbnail;
 export const DEFAULT_PRODUCT_THUMBNAIL = defaultThumbnail;
 
-export const FOOTER_IMAGE = syscat2;
+export const FOOTER_IMAGE = syscat;
